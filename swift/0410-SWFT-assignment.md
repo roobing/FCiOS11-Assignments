8c858932d28ed0a219b9685d93f0c2b50deb12c0
roobing <roobing11@gmail.com>
2020-04-13 11:16:01 +0900

    feat: assignment number 3 solved

--- swift/0410-SWFT-assignment.md
+++ swift/0410-SWFT-assignment.md
@@ -19,15 +19,43 @@
 - 정수를 하나 입력받아 2의 배수 여부를 반환하는 함수
 
   ```swift
-  var abc: Int = 123;
-  var def: Double = 123.123;
+  var num = 0;
+  func multiple2(_ x: Int) {
+      var rest: Int = 0;
+      rest = x % 2;
+      if (rest == 0){
+          print("\(x) is multiple of 2")
+      }
+      else {
+          print("\(x) is not multiple of 2")
+      }
+  }
+  
+  multiple2(3)
   ```
 
   
 
 - 정수를 두 개 입력 받아 곱한 결과를 반환하는 함수 (파라미터 하나의 기본 값은 10)
 
+  ```swift
+  var num1: Int = 10;
+  var num2: Int = 5;
+  
+  func multiply(_ x1: Int, _ x2: Int) {
+      var result: Int = 0;
+      result = x1 * x2
+      print("\(x1) * \(x2) = \(result)")
+  }
+  
+  multiply(num1, num2)
+  ```
+
+  
+
 - 4과목의 시험 점수를 입력받아 평균 점수를 반환하는 함수
+
+  
 
 - 점수를 입력받아 학점을 반환하는 함수 만들기 (90점 이상 A, 80점 이상 B, 70점 이상 C, 그 이하 F)
 
--- swift/0410-SWFT-assignment.playground/Contents.swift
+++ swift/0410-SWFT-assignment.playground/Contents.swift
@@ -1,17 +1,37 @@
 import UIKit
 //assignment 1
-var str = "Hello, playground"
+let name: String = ""
+let age: Int = 0
+func introduceMyself(_ name: String, _ age: Int) {
+  print("Hi! My name is \(name) and I am \(age) years old.")
+}
+
+introduceMyself("Woobin", 34)
 
 
 //assignment 2
-let x: Int = 1;
-var y: Double = 3.14;
-let z: Float = 49.2;
-
-func sum(_ x: Int, _  y: Double, _ z: Float) {
-    print(x, y, z)
+var num = 0;
+func multiple2(_ x: Int) {
+    var rest: Int = 0;
+    rest = x % 2;
+    if (rest == 0){
+        print("\(x) is multiple of 2")
+    }
+    else {
+        print("\(x) is not multiple of 2")
+    }
 }
 
-sum(x, y, z)
+multiple2(3)
 
-//assing
+//assignment 3
+var num1: Int = 10;
+var num2: Int = 5;
+
+func multiply(_ x1: Int, _ x2: Int) {
+    var result: Int = 0;
+    result = x1 * x2
+    print("\(x1) * \(x2) = \(result)")
+}
+
+multiply(num1, num2)
